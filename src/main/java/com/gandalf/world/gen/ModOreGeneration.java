e7376d4a51d64c819fc48448c59265a5f8384b12
ContainmentCreations <patrykbialek.2005@gmail.com>
2025-06-15 18:53:38 +0200

    Add files via upload

---
+++ src/main/java/com/gandalf/world/gen/ModOreGeneration.java
@@ -0,0 +1,18 @@
+package com.gandalf.world.gen;
+
+import com.gandalf.world.feature.ModPlacedFeatures;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.tag.BiomeTags;
+import net.minecraft.world.gen.GenerationStep;
+
+public class ModOreGeneration {
+    public static void generateOres() {
+        System.out.println("✔️ Generating Permafrost Ore in snowy biomes...");
+        BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_TAIGA),
+                GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.PERMAFROST_ORE_PLACED.getKey().get());
+    }
+}
+
+
+
---
+++ src/main/java/com/gandalf/world/gen/ModWorldGen.java
@@ -0,0 +1,7 @@
+package com.gandalf.world.gen;
+
+public class ModWorldGen {
+    public static void generateModWorldGen() {
+        ModOreGeneration.generateOres();
+    }
+}
