ed159490af5c32e701782bdbcf5ca7a7be562b85
ContainmentCreations <patrykbialek.2005@gmail.com>
2025-06-15 18:54:00 +0200

    Add files via upload

--- src/main/java/com/gandalf/CenozoicKingdom.java
+++ src/main/java/com/gandalf/CenozoicKingdom.java
@@ -4,6 +4,8 @@ import com.gandalf.entity.ModBlockEntities;
 import com.gandalf.screen.AnalyzerScreen;
 import com.gandalf.screen.ModScreenHandlers;
 import com.gandalf.screen.SynthetizerScreen;
+import com.gandalf.world.feature.ModConfiguredFeatures;
+import com.gandalf.world.gen.ModWorldGen;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import org.slf4j.Logger;
@@ -22,13 +24,17 @@ public class CenozoicKingdom implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		ModConfiguredFeatures.registerConfiguredFeatures();
+		ModBlocks.registerModBlocks();
+
 
 		LOGGER.info("Hello Fabric world!");
 		ModItems.registerModItems();
-		ModBlocks.registerModBlocks();
 		ModBlockEntities.registerAllBlockEntities();
 
 		ScreenRegistry.register(ModScreenHandlers.ANALYZER_SCREEN_HANDLER, AnalyzerScreen::new);
 		ScreenRegistry.register(ModScreenHandlers.SYNTHETIZER_SCREEN_HANDLER, SynthetizerScreen::new);
+
+		ModWorldGen.generateModWorldGen();
 	}
 }
