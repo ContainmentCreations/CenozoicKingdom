0fd04ed2777960408ddb6c29f5833cc77521d191
ContainmentCreations <patrykbialek.2005@gmail.com>
2025-06-16 18:00:37 +0200

    Update ModPlacedFeatures.java
    lowered attempts on spawn in chunk from 25 to 15

--- src/main/java/com/gandalf/world/feature/ModPlacedFeatures.java
+++ src/main/java/com/gandalf/world/feature/ModPlacedFeatures.java
@@ -8,10 +8,10 @@ import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
 
 public class ModPlacedFeatures {
     public static final RegistryEntry<PlacedFeature> PERMAFROST_ORE_PLACED = PlacedFeatures.register("permafrost_ore_placed",
-            ModConfiguredFeatures.PERMAFROST_ORE, ModOreFeatures.modifiersWithCount(25,
+            ModConfiguredFeatures.PERMAFROST_ORE, ModOreFeatures.modifiersWithCount(15,
                     HeightRangePlacementModifier.trapezoid(YOffset.aboveBottom(32), YOffset.aboveBottom(96))));
 
     public static final RegistryEntry<PlacedFeature> FOSSIL_ORE_PLACED = PlacedFeatures.register("fossil_ore_placed",
-        ModConfiguredFeatures.FOSSIL_ORE, ModOreFeatures.modifiersWithCount(25,
+        ModConfiguredFeatures.FOSSIL_ORE, ModOreFeatures.modifiersWithCount(15,
             HeightRangePlacementModifier.trapezoid(YOffset.fixed(32), YOffset.aboveBottom(96))));
 }
